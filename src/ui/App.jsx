72e179ad17b9738924f6a8eab52b82f7846267ab
Dave.Potter <dave@datonalabs.org>
2023-09-25 12:29:36 +0100

    display publish button when wallet is connected

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -3,9 +3,11 @@ import "./style.css";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import logo from "../assets/img/logo.png";
 import { HomeScreen } from "./screens/HomeScreen";
+import { useAccount } from "wagmi";
 
 export const App = () => {
 
+  const { isConnected } = useAccount()
   const [screen, setScreen] = useState('home');
 
   return (
@@ -13,6 +15,7 @@ export const App = () => {
       <div className="header">
         <img className="logo clickable" src={logo} onClick={() => setScreen('home')} ></img>
         <span className="expander"></span>
+        {isConnected && <span className="header-link">Publish</span>}
         <ConnectButton showBalance={false} chainStatus="none" />
       </div>
       {screen === 'home' && <HomeScreen></HomeScreen>}
